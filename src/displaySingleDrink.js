c26bf5f66a758698e4c9100b9a9ef08cb5b49d25
dejanV91 <dejanvukoicic91@gmail.com>
2022-08-21 08:44:27 +0200

    initial commit

--- src/displaySingleDrink.js
+++ src/displaySingleDrink.js
@@ -1,6 +1,32 @@
-// import { hiddenLoading } from "./toggleLoading.js";
-// import getElement from "./getElement.js";
-// const displayDrink = (data) => {
-//     hiddenLoading();
-    
-// };
+
+import getElement from "./getElement.js";
+const displayDrink = (data) => {
+    const drink = data.drinks[0];
+    const { strDrinkThumb: image, strDrink: name, strInstructions: desc } = drink;
+    const list = [
+        drink.strIngredient1,
+        drink.strIngredient2,
+        drink.strIngredient3,
+        drink.strIngredient4,
+        drink.strIngredient5,
+    ];
+
+    const img = getElement("#img-single");
+    const drinkName = getElement(".single-title");
+    const description = getElement(".single-p");
+    const ingredients = getElement(".single-text");
+    img.src = image;
+    document.title = name;
+    drinkName.textContent = name;
+    description.textContent = desc;
+    ingredients.innerHTML = list
+        .map((item) => {
+            if (!item) return;
+            return `<div class="single-check">
+                        <span class="material-symbols-outlined">check_box</span>
+                        <p class="text">${item}</p>
+                    </div>`;
+        }).join(" "); 
+};
+
+export default displayDrink;
