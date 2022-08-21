414c62545f29607bce1b9f0d93ad7c735cf8ca10
dejanV91 <dejanvukoicic91@gmail.com>
2022-08-21 12:48:04 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a";
 
-// import fetchDrinks from "./src/fetchDrinks.js";
+import fetchDrinks from "./src/fetchDrinks.js";
+import getElement from "./src/getElement.js";
 
-// const presentDrinks = async () => {
-//     const data = await fetchDrinks(URL);
-//     console.log(data);
-// };
+const products = getElement(".products");
+console.log(products);
 
-// present();
+const presentDrinks = async () => {
+    const data = await fetchDrinks(URL);    
+    const drinks = data.drinks;
+    
+    const listDrinks = drinks
+        .map((item) => {
+            const {strDrinkThumb: image, strDrink: name, idDrink: id} = item;
+            if (item) return;
+            return `<a class="linkCocktails" href="products.html" data-id=${id}>
+                        <article class="item">
+                            <div class="imageDiv">
+                                <img class="image" src="${image}" alt="${name}">
+                            </div>
+                            <p class="title">${name}</p>
+                        </article> 
+                    </a>`;
+        }).join("");
+    products.innerHTML = listDrinks();
+};
+
+presentDrinks();
 
